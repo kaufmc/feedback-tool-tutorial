7121aef518fd410fa02ce95197d182a15404e253
Christian Kaufmann <kaufmc@gmx.at>
2022-06-30 20:41:46 +0200

    Display feedback items

---
+++ src/components/Card.svelte
@@ -0,0 +1,14 @@
+<div class="card">
+  <slot></slot>
+</div>
+
+<style>
+  .card{
+    background-color: #ffffff;
+    border-radius: 15px;
+    padding: 40px 50px;
+    margin: 20px 0px;
+    position: relative;
+    color: #333333;
+  }
+</style>
